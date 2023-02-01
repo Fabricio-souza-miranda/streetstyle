e9afad488c408b3bc64ab12947cd647b39581af3
Fabrício <dev.fabriciosm@gmail.com>
2023-01-31 21:44:35 -0300

    terminando validação das paginas de cadastro e de login

--- scripts/login.js
+++ scripts/login.js
@@ -29,8 +29,6 @@ passwordShow.addEventListener("click", () =>{
 const form = document.getElementById("form");
 const button = document.getElementById("button");
 const link = document.querySelector("#button a");
-const userName = document.getElementById("username");
-const userLastName = document.getElementById("lastname");
 const userEmail = document.getElementById("email");
 const userPassword = document.getElementById("password");
 const required = document.querySelectorAll(".required");
@@ -48,34 +46,11 @@ required.forEach((input) => {
  })
 
  function checkInputs() {
-   const userNameValue = userName.value;
-   const userLastNameValue = userLastName.value;
    const userEmailValue = userEmail.value;
    const userPasswordValue = userPassword.value;
    const regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g;
-   const regexName = /\d/g;
+
    
-
-   if (userNameValue === "") {
-     setErrorFor(userName, 'Insira seu nome.');
-   } else if(regexName.test(userNameValue)){
-     setErrorFor(userName, 'Coloque somente letras.');
-   } else if(userNameValue.length < 3){
-     setErrorFor(userName, 'No mínimo 3 letras.');
-   } else {
-     setSuccessFor(userName);
-   }
-
-   if (userLastNameValue === "") {
-     setErrorFor(userLastName, 'Insira seu sobrenome.');
-   } else if(regexName.test(userLastNameValue)){
-     setErrorFor(userLastName, 'Coloque somente letras.');
-   } else if(userLastNameValue.length < 3){
-     setErrorFor(userLastName, 'No mínimo 3 letras.');
-   } else {
-     setSuccessFor(userLastName);
-   }
-
    if(userEmailValue === ""){
      setErrorFor(userEmail, 'Por favor, insira um email.');
    } else if(regex.test(userEmailValue)){
